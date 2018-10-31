18ad7c68404fc66b4b631adb1b4a79df0e0c91b2
Ynnk <y.houssin@live.fr>
2018-10-31 11:59:51 +0100

    adding design to the card

--- lib/main.dart
+++ lib/main.dart
@@ -18,60 +18,73 @@ class MyApp extends StatelessWidget {
         body: Card(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Text(
-                      'Aloe Vera',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 20.0),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+                    Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  'Aloe Vera',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(15.0),
+                constraints: BoxConstraints.expand(
+                  height: Theme.of(context).textTheme.display1.fontSize * 1.1 +
+                      120.0,
+                ),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5.0),
+                    image: DecorationImage(
+                      image: ExactAssetImage('assets/aloe.jpg'),
+                      fit: BoxFit.cover,
+                    )),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Next Watering in',
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.white),
+                          ),
+                          Text(
+                            '8 days',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0,
+                                color: Colors.white),
+                          ),
+                          DecoratedBox(
+                            decoration: BoxDecoration(
+                                //color: Colors.lightGreen
+                                gradient: LinearGradient(
+                                    begin: FractionalOffset.bottomLeft,
+                                    end: FractionalOffset.topRight,
+                                    colors: [
+                                  Colors.black,
+                                  Colors.transparent,
+                                ])),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(15.0),
-                    constraints: BoxConstraints.expand(
-                      height:
-                          Theme.of(context).textTheme.display1.fontSize * 1.1 +
-                              120.0,
-                    ),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5.0),
-                        image: DecorationImage(
-                          image: ExactAssetImage('assets/aloe.jpg'),
-                          fit: BoxFit.cover,
-                        )),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          'Next Watering in',
-                          style: TextStyle(fontSize: 18.0, color: Colors.white),
-                        ),
-                        Text(
-                          '8 days',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
-                              color: Colors.white),
-                        ),
-                        DecoratedBox(
-                          decoration: BoxDecoration(
-                              //color: Colors.lightGreen
-                              gradient: LinearGradient(
-                                  begin: FractionalOffset.bottomLeft,
-                                  end: FractionalOffset.topRight,
-                                  colors: [
-                                Colors.black,
-                                Colors.transparent,
-                              ])),
-                        ),
-                      ],
-                    ),
-                  )
-                ]),
+                    Container(
+                      alignment: Alignment.topCenter,
+                      child: Image.asset(
+                        'assets/drop-icon--white.png',
+                        width: 30.0,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ]),
           ),
         ),
       ),
