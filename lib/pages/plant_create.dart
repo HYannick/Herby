b0962d1df7e02014a89aecb3fb4766d72ad750db
ayho <y.houssin01@gmail.com>
2018-11-05 08:18:59 +0100

    adding textfield

--- lib/pages/plant_create.dart
+++ lib/pages/plant_create.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
-class PlantCreatePage extends StatelessWidget {
+class PlantCreatePage extends StatefulWidget {
+  @override
+  PlantCreatePageState createState() {
+    return new PlantCreatePageState();
+  }
+}
+
+class PlantCreatePageState extends State<PlantCreatePage> {
+  String titleText = '';
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Plant Create page'),
+    return Column(
+      children: <Widget>[
+        TextField(
+          onChanged: (String value) {
+            setState(() {
+              titleText = value;
+            });
+          },
+        ),
+        Text(titleText)
+      ],
     );
   }
 }
